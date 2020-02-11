3df393a9d8c26401e09a4435ddd7d18560be1622
DonPetrelone <vinicius.petrellicicerone@wmich.edu>
2020-02-11 16:12:58 -0500

    changes to source files and added soil moisture sensor code

--- src/main.py
+++ src/main.py
@@ -14,7 +14,7 @@ airSensor = 22
 airPort = 4
 lightPin = 7
 
-'''
+
 temp, hum = AirTempHum.airTempHum(airSensor, airPort)
 
 if hum is not None and temp is not None:
@@ -22,6 +22,5 @@ if hum is not None and temp is not None:
     print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temp, hum))
 else:
     print('Failed to get reading. Try again!')
-'''
 
-VoltageControl.pinVoltage(lightPin, 1, 1)
+#VoltageControl.pinVoltage(lightPin, 1, 0)
