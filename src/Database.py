afe7b532a7d7a3b71de81af0b4831b98bfe37fea
Vinicius Petrelli Cicerone <petrelone@Viniciuss-MacBook-Pro.local>
2020-02-14 08:56:03 -0500

    commit changes to database file - database connection and insert query are performed in the method

--- src/Database.py
+++ src/Database.py
@@ -5,3 +5,22 @@
 #This file contains a method that connects to the database, performs an insert query than disconnects.
 
 import sqlite3
+
+def dbConnect(airTemp, airHum, soil, dateTime):
+
+    #create database connection
+    conn = sqlite3.connect('HerbControl.db')
+
+    #cursor object which allows the execution of queries
+    c = conn.cursor()
+
+    #Execute the insert query with the parameters
+    c.execute("INSERT INTO ...")
+
+    #commit changes
+    conn.commit()
+
+    #close connection
+    conn.close()
+
+    return
--- src/SoilMoisture.py
+++ src/SoilMoisture.py
@@ -7,12 +7,13 @@ delay = 0.2
 spi = spidev.SpiDev()
 spi.open(0,0)
 
+'''
 def readChannel(channel):
 	val = spi.xfer2([1,(8+channel)<<4,0])
 	data = ((val[1]&3) << 8) + val[2]
 	return data
-
-if __name__ == "__main__":
+	
+	if __name__ == "__main__":
 	try:
 		while True:
 			val = readChannel(0)
@@ -21,3 +22,4 @@ if __name__ == "__main__":
 
 	except KeyboardInterrupt:
 		print "Cancel."
+'''
