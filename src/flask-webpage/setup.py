a1954e01095742ac7b993d3183f56e838f72fa5c
Vinicius Petrelli Cicerone <petrelone@Viniciuss-MacBook-Pro.local>
2020-03-30 17:23:28 -0400

    commit

---
+++ src/flask-webpage/setup.py
@@ -0,0 +1,19 @@
+import io
+
+from setuptools import find_packages
+from setuptools import setup
+
+with io.open("README.rst", "rt", encoding="utf8") as f:
+    readme = f.read()
+
+setup(
+    name="flaskr",
+    version="1.0.0",
+    license="BSD",
+    long_description=readme,
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=["flask"],
+    extras_require={"test": ["pytest", "coverage"]},
+)
