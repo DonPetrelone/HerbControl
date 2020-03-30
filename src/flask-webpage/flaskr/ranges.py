35517d8740dcd6f8c2813ebb96861c368a3bf55c
Vinicius Petrelli Cicerone <petrelone@Viniciuss-MacBook-Pro.local>
2020-03-30 13:07:24 -0400

    commit changes to the webpage

--- src/flask-webpage/flaskr/ranges.py
+++ src/flask-webpage/flaskr/ranges.py
@@ -19,12 +19,35 @@ def get_values():
 	vals = get_db().execute('SELECT * FROM ranges').fetchone()	
 	return vals
 
-@bp.route('/update')
-def update(athd, athw, atg, atlw, atld, ahhd, ahhw, ahg, ahlw, ahld, smhd, smhw, smg, smlw, smld):
-	db = get_values()
-	db.execute('UPDATE ranges SET airTempHighDanger = ?, airTempHighWarning = ?, airTempGood = ?, airTempLowWarning = ?, airTempLowDanger = ?, airHumHighDanger = ?,airHumHighWarning = ?, airHumGood = ?, airHumLowWarning = ?, airHumLowDanger = ?, soilMoistHighDanger = ?, soilMoistHighWarning = ?, soilMoistGood = ?, soilMoistLowWarning = ?, soilMoistLowDanger = ? ',
-	(athd, athw, atg, atlw, atld, ahhd, ahhw, ahg, ahlw, ahld, smhd, smhw, smg, smlw, smld)
-	)
-	db.commit()
+@bp.route('/update', methods = ['POST', 'GET'])
+@login_required
+def update():
+	
+	if request.method == 'POST':
+		try:
+			athd = request.form['airTempHighDanger']
+			athw = request.form['airTempHighWarning']
+			atg = request.form['airTempGood']
+			atlw = request.form['airTempLowWarning']
+			atld = request.form['airTempLowDanger']
+			ahhd = request.form['airHumHighDanger']
+			ahhw = request.form['airHumHighWarning']
+			ahg = request.form['airHumGood']
+			ahlw = request.form['airHumLowWarning']
+			ahld = request.form['airHumLowDanger']
+			smhd = request.form['soilMoistHighDanger']
+			smhw = request.form['soilMoistHighWarning']
+			smg = request.form['soilMoistGood']
+			smlw = request.form['soilMoistLowWarning']
+			smld = request.form['soilMoistLowDanger']
+			db = get_db()
+			db.execute('UPDATE ranges SET airTempHighDanger = ?, airTempHighWarning = ?, airTempGood = ?, airTempLowWarning = ?, airTempLowDanger = ?, airHumHighDanger = ?,airHumHighWarning = ?, airHumGood = ?, airHumLowWarning = ?, airHumLowDanger = ?, soilMoistHighDanger = ?, soilMoistHighWarning = ?, soilMoistGood = ?, soilMoistLowWarning = ?, soilMoistLowDanger = ? ',
+			(athd, athw, atg, atlw, atld, ahhd, ahhw, ahg, ahlw, ahld, smhd, smhw, smg, smlw, smld)
+			)
+			db.commit()
+		except:
+			db.rollback()
 
-	return render_template('ranges/index.html')
+		finally:
+			return redirect(url_for('index'))
+			db.close()
