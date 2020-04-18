72e6bac370d9bd72802a727595c7b8c80f10dff4
Vinicius Petrelli Cicerone <petrelone@Viniciuss-MacBook-Pro.local>
2020-04-18 14:22:29 -0400

    Main directory READEME

---
+++ src/Watchdog.py
@@ -0,0 +1,6 @@
+#Herb Control - Senior Design Project
+#Western Michigan University
+#Authors: Vinicius Petrelli Cicerone, Dylan Lafleur, Paxton Plum
+#Faculty Advisor: Dr. John Kapenga
+#This file is the watchdog for the system and works together with the
+#sensors file.
--- src/flask-webpage/tests/conftest.py
+++ src/flask-webpage/tests/conftest.py
@@ -2,6 +2,7 @@ import os
 import tempfile
 
 import pytest
+
 from flaskr.db import get_db, init_db
 from flaskr import create_app
 
